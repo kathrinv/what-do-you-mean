8f8d21bfb3ffc704891689182d88b795cb9eb15b
kathrin v <go.kathrin@gmail.com>
2020-02-11 18:27:36 -0500

    added LDA modeling functions

--- frb_functions.py
+++ frb_functions.py
@@ -3,22 +3,25 @@
 from selenium import webdriver
 import re
 import time
+    # data analysis
 import numpy as np
 import pandas as pd
-
-#natural language processing
+import pickle
+from tqdm import tqdm_notebook as tqdm
+    # natural language processing - NLTK
 import nltk
 nltk.download('wordnet')
 from nltk.corpus import wordnet, stopwords
 from nltk.probability import FreqDist
 from nltk.stem import WordNetLemmatizer 
-
-from tqdm import tqdm_notebook as tqdm
+    # natural language processing - Gensim and LDA
 import gensim
 from gensim import corpora, models, similarities
 from gensim.models import CoherenceModel
 import pyLDAvis.gensim
-
+    # natural language processing - TextBlob (Sentiment)
+from textblob import TextBlob
+    # data visualization
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import seaborn as sns
@@ -56,6 +59,9 @@ def get_frb_article_links(browser):
     """
     Helper function for navigagte_frb_speeches.
     (only works for non-archived speeches)
+    
+    Parameters:
+    browser: Selenium browser instance
     
     Returns:
     list: Speech urls for the current
@@ -107,6 +113,18 @@ def get_frb_speech_text(url_lst):
     return frb_articles
 
 def get_frb_article_links_archived(browser):
+    """
+    Helper function for navigagte_frb_archived speeches.
+    (only works for archived speeches)
+    
+    Parameters:
+    browser: Selenium browser instance
+    
+    Returns:
+    list: Speech urls, titles, speakers
+    locations, and dates for the current
+    page of speeches.
+    """
     new_urls = []
     new_titles = []
     new_speakers = []
@@ -132,6 +150,17 @@ def get_frb_article_links_archived(browser):
     return new_urls, new_titles, new_speakers, new_locations, new_dates
 
 def navigate_frb_archived_speeches():
+    """
+    Navigates the archived Fed Speeches website 
+    and calls get_frb_article_links_archiged helper
+    function to scrape the urls to all Fed 
+    speeches from the Fed website (archived
+    speeches up until 1996). 
+    
+    Returns:
+    list: Speech urls for all non-archived
+    speeches on the Feb website.
+    """
     # initiating selenium Chrome webdriver instance
     option = webdriver.ChromeOptions()
     option.add_argument(" — incognito")
@@ -165,6 +194,17 @@ def navigate_frb_archived_speeches():
     return speech_urls, speakers, locations, dates_, titles
 
 def get_frb_speech_text_archived(url_lst):
+    """
+    Accesses and scrapes all the speech text from a
+    list of urls provided. Only works for archived
+    speeches on the Fed website.
+    
+    Parameters: 
+    url_lst (list): list of speech urls to scrape
+    
+    Returns:
+    list: speech text
+    """
     # initiating selenium Chrome webdriver instance
     option = webdriver.ChromeOptions()
     option.add_argument(" — incognito")
@@ -181,9 +221,12 @@ def get_frb_speech_text_archived(url_lst):
     browser.close()
     return speech_text
 
+
+# webscraping functions for FOMC speeches
+# project expansion
+# not used in current project
+
 def navigate_fomc_speeches():
-    """
-    """
     fomc_urls = []
     # initiating selenium Chrome webdriver instance
     option = webdriver.ChromeOptions()
@@ -201,8 +244,6 @@ def navigate_fomc_speeches():
     return fomc_urls
 
 def get_fomc_article_links(browser):
-    """
-    """
     new_urls = []
     speeches = browser.find_elements_by_class_name('itemTitle')
     for speech in speeches:
@@ -211,9 +252,6 @@ def get_fomc_article_links(browser):
     return new_urls
 
 def get_fomc_speech_text(url_lst):
-    """
-
-    """
     option = webdriver.ChromeOptions()
     option.add_argument(" — incognito")
     browser = webdriver.Chrome(options=option)
@@ -235,7 +273,6 @@ def get_fomc_speech_text(url_lst):
 
 
 def navigate_fomc_archived_speeches():
-    # initiating selenium Chrome webdriver instance
     option = webdriver.ChromeOptions()
     option.add_argument(" — incognito")
     browser = webdriver.Chrome(options=option)
@@ -329,9 +366,20 @@ def get_fed_funds_rates(archived=False):
     return dates_, inc, dec, target
 
 
-
-# Text cleaning
+# Data Cleaning
 def remove_references(text):
+    """
+    Removes references at the end of
+    speeches, if any. Helper function
+    to assist in data cleaning.
+    
+    Parameters: 
+    text (string): speech text
+    
+    Returns:
+    string: cleaned speech text sans
+          references
+    """
     references_loc = text.find('\nReferences\n')
     if references_loc != -1:
         text = text[:references_loc]
@@ -344,22 +392,40 @@ def remove_references(text):
     return text
 
 def clean_speech_text(df):
+    """
+    Cleans speech text, removing
+    urls, links, numbers, references,
+    and special characters.
+    
+    Parameters: 
+    df (DataFrame): FRB speech df
+        with "full_text" column to
+        be cleaned
+    
+    Returns:
+    DataFrame: pandas DataFrame with
+        "full_text" column cleaned
+    """
     df_new = df.copy()
-    df_new.loc['full_text'] = df_new['full_text'].apply(lambda x: remove_references(x))
-    df_new.loc['full_text'] = df_new['full_text'].str.replace('\n', ' ')
-    df_new.loc['full_text'] = df_new['full_text'].apply(lambda x: re.sub(r'(http)\S+(htm)(l)?', '', x))
-    df_new.loc['full_text'] = df_new['full_text'].apply(lambda x: re.sub(r'(www.)\S+', '', x))
-    df_new.loc['full_text'] = df_new['full_text'].apply(lambda x: re.sub(r'[\d]', '', x))
-    df_new.loc['full_text'] = df_new['full_text'].str.replace('—', ' ')
-    df_new.loc['full_text'] = df_new['full_text'].str.replace('-', ' ')
-    df_new.loc['full_text'] = df_new['full_text'].apply(lambda x: re.sub(r'[^\w\s]', '', x))
-    df_new.loc['full_text'] = df_new['full_text'].apply(lambda x: re.sub(r'([Rr]eturn to text)', '', x))
-    df_new.loc['full_text'] = df_new['full_text'].apply(lambda x: re.sub(r'([Pp]lay [vV]ideo)', '', x))
+    full_text_col = df_new['full_text'].apply(lambda x: remove_references(x))
+    full_text_col = full_text_col.str.replace('\n', ' ')
+    full_text_col = full_text_col.apply(lambda x: re.sub(r'(http)\S+(htm)(l)?', '', x))
+    full_text_col = full_text_col.apply(lambda x: re.sub(r'(www.)\S+', '', x))
+    full_text_col = full_text_col.apply(lambda x: re.sub(r'[\d]', '', x))
+    full_text_col = full_text_col.str.replace('—', ' ')
+    full_text_col = full_text_col.str.replace('-', ' ')
+    full_text_col = full_text_col.apply(lambda x: re.sub(r'[^\w\s]', '', x))
+    full_text_col = full_text_col.apply(lambda x: re.sub(r'([Rr]eturn to text)', '', x))
+    full_text_col = full_text_col.apply(lambda x: re.sub(r'([Pp]lay [vV]ideo)', '', x))
+    df_new.drop(labels='full_text', axis="columns", inplace=True)
+    df_new['full_text'] = full_text_col
     return df_new
 
 
 def get_wordnet_pos(word):
-    """Map POS tag to first character lemmatize() accepts"""
+    """
+    Maps POS tag to word token
+    """
     tag = nltk.pos_tag([word])[0][1][0].upper()
     tag_dict = {"J": wordnet.ADJ,
                 "N": wordnet.NOUN,
@@ -370,23 +436,33 @@ def get_wordnet_pos(word):
 
 
 def lemmatize_speech_text(text):
+    """
+    Lemmatizes text based on Part
+    of Speech (POS) by tokenizing words,
+    finding the POS, and passing the 
+    POS and token into nltk's lemmatizer.
+    
+    Parameters: 
+    text (string): speech text
+    
+    Returns:
+    list: lemmatized tokens
+    """
     lemmatizer = WordNetLemmatizer()
     tokens_lower = [w.lower() for w in nltk.word_tokenize(text)]
     return [lemmatizer.lemmatize(w, get_wordnet_pos(w)) for w in tokens_lower]
 
-def count_unique_words(text):
-    return len(set(text))
-
-def tokenize_and_remove_stopwords(text):
-    tokens = word_tokenize(text)
-    stopped_tokens = [w for w in tokens if w not in stopwords_without_punct]
-    return stopped_tokens
-
-def get_most_common_words(tokens, num=20):
-    fdist = FreqDist(tokens)
-    return fdist.most_common(num)
-
 def remove_stop_words(tokens_list):
+    """
+    Removes English stop words
+    from list of tokens
+    
+    Parameters: 
+    tokens_list (list): list of words
+    
+    Returns:
+    list: list of words sans stop words
+    """
     stopwords_without_punct = []
     for word in stopwords.words('english'):
         word = word.replace("'", "")
@@ -394,14 +470,87 @@ def remove_stop_words(tokens_list):
     stopped_tokens = [w for w in tokens_list if w not in stopwords_without_punct]
     return [w for w in stopped_tokens if len(w) > 2]
 
+
+def count_unique_words(text):
+    """
+    Counts number of unqiue
+    words in a piece of text
+    
+    Parameters: 
+    text (string): speech text
+    
+    Returns:
+    int: number of unique words
+    """
+    return len(set(text))
+
+# Old function - replaced by lemmatize_speech_text() and remove_stop_words()
+# def tokenize_and_remove_stopwords(text):
+#     tokens = word_tokenize(text)
+#     stopped_tokens = [w for w in tokens if w not in stopwords_without_punct]
+#     return stopped_tokens
+
+def get_most_common_words(tokens, num=20):
+    """
+    Returns list of a number of
+    most common tokens (words) in 
+    a speech
+    
+    Parameters: 
+    tokens (list): list of tokenized
+        words from a speech
+        
+    num (int): number of top words
+        to return
+    
+    Returns:
+    list of tuples: Words and count
+        of the number of times each
+        word appears
+    """
+    fdist = FreqDist(tokens)
+    return fdist.most_common(num)
+
 def convert_to_datetime(df):
-    df_new = df.copy()
-    df_new.loc['speech_datetime'] = df_new['speech_date'].apply(lambda x: pd.to_datetime(x))
-    df_new.loc['speech_year'] = df_new['speech_datetime'].apply(lambda x: x.year)
-    df_new.loc['speech_month'] = df_new['speech_datetime'].apply(lambda x: x.month)
+    """
+    Creates 3 new columns in 
+    FRB speech df, including speech
+    date, year, and month.
+    
+    Parameters: 
+    df (DataFrame): FRB speech df
+        with "speech_date" column to
+        be parsed
+    
+    Returns:
+    DataFrame: pandas DataFrame with
+        3 new date columns
+    """
+    df_new = df.copy(deep=True)
+    df_new['speech_datetime'] = df_new['speech_date'].apply(lambda x: pd.to_datetime(x))
+    df_new['speech_year'] = df_new['speech_datetime'].apply(lambda x: x.year)
+    df_new['speech_month'] = df_new['speech_datetime'].apply(lambda x: x.month)
     return df_new
 
 def plot_most_common_words(df, article_num=9):
+    """
+    Plots the 20 most common words in 
+    a speech, before and after removing
+    stop words
+    
+    Parameters:
+    df (DataFrame): FRB speech
+        df with 'common_20_stopped_lemm_words'
+        column
+    article_num (int): index number
+        of the speech for which to
+        generate the barplot
+    
+    Returns:
+    Displays 2 sns barplots of top 20
+    words
+    
+    """
     fig = plt.figure(figsize=(15, 6))
     fig.suptitle(f"Most common words in Speech: {df.iloc[article_num]['title']}")
     left = fig.add_subplot(121)
@@ -422,6 +571,122 @@ def plot_most_common_words(df, article_num=9):
     plt.show()
     
 
+def create_dictionary(df, col_name = 'stopped_lemm_words', no_below=10, no_above=0.66, keep_n=10000):
+    """
+    Creates a dictionary for our corpus
+    
+    Parameters:
+    df (DataFrame): df containing the
+        lemmatized and tokenized corpus
+        
+     col_name (str): name of column in
+         the df containing the lemmatized
+         and tokenized corpus
+         
+     no_below (int): Minimum number of documents
+         the word mnust appear in to be included
+         in the corpus
+     
+     no_above (int): Max percentage of documents
+         in the corpus the word can appear in.
+         Otherwise, word is removed from the corpus
+     
+     keep_n (int): Maximum number of 
+         words to keep in the dictionary
+         
+    Returns:
+    dictionary: list of tokens in the
+        dictionary
+        
+    """
+    dictionary = corpora.Dictionary(df[col_name])
+    print(f"Number of words in dictionary prior to filtering: {len(dictionary)}")
+    dictionary.filter_extremes(no_below=10, no_above=0.66, keep_n=10000)
+    print(f"Number of words in dictionary after filtering: {len(dictionary)}")
+    return dictionary
+                                    
+def create_bow(df, dictionary, col_name = 'stopped_lemm_words'):
+    """
+    Creates a dictionary for our corpus
+    
+    Parameters:
+    df (DataFrame): df containing the
+        lemmatized and tokenized corpus
+        
+     col_name (str): name of column in
+         the df containing the lemmatized
+         and tokenized corpus
+         
+     no_below (int): Minimum number of documents
+         the word mnust appear in to be included
+         in the corpus
+     
+     no_above (int): Max percentage of documents
+         in the corpus the word can appear in.
+         Otherwise, word is removed from the corpus
+     
+     keep_n (int): Maximum number of 
+         words to keep in the dictionary
+        
+    """
+    bow_corpus = [dictionary.doc2bow(speech) for speech in df[col_name]]
+    return bow_corpus
 
 
-
+def get_scores(corpus,
+               dictionary,
+               df,
+               col_name,
+               min_num_topics = 2,
+               max_num_topics = 15,
+               passes=10,
+               random_state=100):
+    """
+    
+    """
+    num_topics = list(range(min_num_topics, max_num_topics + 1))
+    coherence_scores = []
+    perplexity_scores = []
+    
+    for num in range(min_num_topics, max_num_topics+1):
+        lda_model = gensim.models.LdaMulticore(corpus, num_topics=num, id2word=dictionary,
+                                               random_state = random_state, passes = passes)
+        perplexity_scores.append(lda_model.log_perplexity(corpus))
+        
+        coherence_model_lda = CoherenceModel(model=lda_model, texts=df[col_name], dictionary=dictionary,
+                                     coherence='c_v')
+        coherence_scores.append(coherence_model_lda.get_coherence())
+               
+    data = {'num_topics': num_topics, 'coherence': coherence_scores, 'perplexity': perplexity_scores}
+    return pd.DataFrame(data)
+                 
+def run_and_save_final_lda_model(corpus,
+                                 dictionary,
+                                 df,
+                                 col_name,
+                                 num_topics = 11,
+                                 passes = 10,
+                                 random_state = 100):
+    """
+    
+    """
+    # fit the lda model
+    lda_model = gensim.models.LdaMulticore(bow_corpus, num_topics=num_topics, id2word=dictionary,
+                                       random_state = random_state, passes = passes)
+    
+    # pickle the lda model
+    pickle.dump(lda_model, open('lda_model' + str(num_topics) + '.sav', 'wb'))
+    
+    # create the visualization
+    vis = pyLDAvis.gensim.prepare(lda_model, corpus, dictionary=lda_model.id2word)
+    # save the visualization in html format
+    pyLDAvis.save_html(vis, 'lda_' + str(num_topics) + '_topics.html')
+    
+    # get the dominant topic information
+    df_dominant = get_dominant_topic(ldamodel=lda_model, corpus=bow_corpus)
+    # pickle the dominant topics
+    df_dominant.to_pickle('df_dominant_' + str(num_topics) + '_topics.pkl')
+    
+    return lda_model, df_dominant
+                 
+           
